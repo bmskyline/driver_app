7e580c3e60dd9b7964a8d6604842432677d6d312
TienManh <tienmanbmt@gmail.com>
2020-01-11 11:39:31 +0700

    change setState to provider

--- lib/view/home/new/new_provider.dart
+++ lib/view/home/new/new_provider.dart
@@ -7,11 +7,14 @@ import 'package:rxdart/rxdart.dart';
 class NewProvider extends BaseProvider {
   final GithubRepo _repo;
   bool _loading = false;
-  List<User> _response = List();
+  List<User> _response = [];
 
   NewProvider(this._repo);
 
-  List<User> get response => _response;
+  List<User> get response {
+    print(_response.length.toString());
+    return _response;
+  }
   set response(List<User> response) {
     _response = response;
     notifyListeners();
@@ -27,8 +30,7 @@ class NewProvider extends BaseProvider {
       .getUsers()
       .doOnData((r) {
         print("data bao");
-          _response.addAll((r as List).map((user) => User.fromJson(user)).toList());
-          notifyListeners();
+          _response = (r as List).map((user) => User.fromJson(user)).toList();//.addAll((r as List).map((user) => User.fromJson(user)).toList());
   })
       .doOnError((e, stacktrace) {
         if (e is DioError) {
